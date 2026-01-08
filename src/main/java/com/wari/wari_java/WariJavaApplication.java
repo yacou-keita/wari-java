abe773abda4253e09c55735ea238948da2a30669
= <yacoukeita0212@gmail.com>
2026-01-08 21:25:48 +0000

    project iniatialization

---
+++ src/main/java/com/wari/wari_java/WariJavaApplication.java
@@ -0,0 +1,13 @@
+package com.wari.wari_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WariJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WariJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wari/wari_java/WariJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wari.wari_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WariJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
